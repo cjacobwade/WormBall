fb89d568bf99cdbc02e742f151baaef77c8d1faa
Chris Wade <cjacobwade@gmail.com>
2015-01-23 16:42:14 -0600

    Better input, team color swapping, actual textures

--- WormBall/Assets/Scripts/CharacterSelect.cs
+++ WormBall/Assets/Scripts/CharacterSelect.cs
@@ -22,27 +22,32 @@ public class CharacterSelect : MonoBehaviour
 {
 	[SerializeField] PlayerInfo[] playerInfos;
 
+	[SerializeField] Sprite defaultPlayerSprite;
+	Color defaultSpriteColor;
+
 	[SerializeField] Sprite[] playerSprites;
 	List<int>[] teamOpenSpriteIndices = new List<int>[2];
 	
-	List<int> openTeamColorIndices; // available team colors
+	List<int> openTeamColorIndices = new List<int>(); // available team colors
 
-	int[] teamColorIndices; // colorIndex for each team
+	int[] teamColorIndices = new int[2]; // colorIndex for each team
 
 	[SerializeField] float inputTime;
 
 	void Awake()
 	{
+		defaultSpriteColor = playerInfos[0].image.color;
+
 		for(int i = 0; i < 2; i++)
 		{
 			teamOpenSpriteIndices[i] = new List<int>();
+			teamColorIndices[i] = i;
 		}
 
 		for(int i = 0; i < 8; i++)
 		{
 			playerInfos[i].joined = false;
 			playerInfos[i].teamIndex = i < 4 ? 0 : 1;
-			playerInfos[i].image.color = GameManager.instance.colorOptions[playerInfos[i].teamIndex];
 		}
 	}
 
@@ -52,6 +57,7 @@ public class CharacterSelect : MonoBehaviour
 		{
 			if(playerInfos[i].inputTimer > inputTime)
 			{
+				// Join Game
 				if(!playerInfos[i].joined && Input.GetButtonDown("Propel_P" + (i + 1) + WadeUtils.platformName))
 				{
 					Debug.Log("Player Join");
@@ -59,6 +65,15 @@ public class CharacterSelect : MonoBehaviour
 					playerInfos[i].inputTimer = 0f;
 				}
 
+				// Leave Game
+				if(playerInfos[i].joined && Input.GetButtonDown("Leave_P" + (i + 1) + WadeUtils.platformName))
+				{
+					Debug.Log("Player Leave");
+					PlayerLeaveGame(i);
+					playerInfos[i].inputTimer = 0f;
+				}
+
+				// Change Texure
 				float scrollInput = Input.GetAxis("Horizontal_P" + (i + 1));
 				if(playerInfos[i].joined && Mathf.Abs(scrollInput) > WadeUtils.SMALLNUMBER)
 				{
@@ -67,11 +82,12 @@ public class CharacterSelect : MonoBehaviour
 					playerInfos[i].inputTimer = 0f;
 				}
 
+				// Change Team Color
 				float bumperInput = Input.GetAxis("Bumper_P" + (i + 1) + WadeUtils.platformName);
 				if(playerInfos[i].joined & Mathf.Abs(bumperInput) > WadeUtils.SMALLNUMBER)
 				{
 					Debug.Log("Change Team Color");
-					ChangeTeamColor(i, bumperInput);
+					ChangeTeamColor(i < 4 ? 0 : 1, bumperInput);
 					playerInfos[i].inputTimer = 0f;
 				}
 			}
@@ -90,7 +106,18 @@ public class CharacterSelect : MonoBehaviour
 		int firstSpriteIndex = teamOpenSpriteIndices[teamIndex][0];
 
 		playerInfos[playerIndex].image.sprite = playerSprites[firstSpriteIndex];
+		playerInfos[playerIndex].image.color = GameManager.instance.colorOptions[teamColorIndices[teamIndex]];
 		teamOpenSpriteIndices[teamIndex].RemoveAt(0);
+	}
+
+	void PlayerLeaveGame(int playerIndex)
+	{
+		PlayerInfo playerInfo = playerInfos[playerIndex];
+
+		playerInfo.joined = false;
+		teamOpenSpriteIndices[playerInfo.teamIndex].Add(playerInfo.spriteIndex);
+		playerInfo.image.sprite = defaultPlayerSprite;
+		playerInfo.image.color = defaultSpriteColor;
 	}
 
 	void CheckAvailableSprites()
@@ -191,12 +218,18 @@ public class CharacterSelect : MonoBehaviour
 		
 		for(int i = 0; i < GameManager.instance.colorOptions.Length; i++)
 		{ 
+			bool colorUsed = false;
 			for(int j = 0; j < teamColorIndices.Length; j++)
 			{
-				if(teamColorIndices[j] != i)
+				if(teamColorIndices[j] == i)
 				{
-					openTeamColorIndices.Add(j);
+					colorUsed = true;
 				}
+			}
+
+			if(!colorUsed)
+			{
+				openTeamColorIndices.Add(i);
 			}
 		}
 	}
@@ -210,6 +243,15 @@ public class CharacterSelect : MonoBehaviour
 		int i = teamColorIndices[teamIndex];
 		while(true)
 		{
+			if(i >= openTeamColorIndices.Count)
+			{
+				i = 0;
+			}
+			else if(i < 0)
+			{
+				i = openTeamColorIndices.Count - 1;
+			}
+
 			if(bumperInput > 0f)
 			{
 				if(openTeamColorIndices[i] != teamColorIndices[teamIndex])
@@ -218,9 +260,12 @@ public class CharacterSelect : MonoBehaviour
 					teamColorIndices[teamIndex] = openTeamColorIndices[i];
 					openTeamColorIndices.Remove(openTeamColorIndices[i]);
 				
-					for(int j = 4 * teamIndex; j < 4 * teamIndex + 1; j++)
+					for(int j = 4 * teamIndex; j < 4 * teamIndex + 4; j++)
 					{
-						playerInfos[j].image.color = GameManager.instance.colorOptions[teamColorIndices[teamIndex]];
+						if(playerInfos[j].joined)
+						{
+							playerInfos[j].image.color = GameManager.instance.colorOptions[teamColorIndices[teamIndex]];
+						}
 					}
 
 					break;
@@ -240,9 +285,12 @@ public class CharacterSelect : MonoBehaviour
 					teamColorIndices[teamIndex] = openTeamColorIndices[i];
 					openTeamColorIndices.Remove(openTeamColorIndices[i]);
 					
-					for(int j = 4 * teamIndex; j < 4 * teamIndex + 1; j++)
+					for(int j = 4 * teamIndex; j < 4 * teamIndex + 4; j++)
 					{
-						playerInfos[j].image.color = GameManager.instance.colorOptions[teamColorIndices[teamIndex]];
+						if(playerInfos[j].joined)
+						{
+							playerInfos[j].image.color = GameManager.instance.colorOptions[teamColorIndices[teamIndex]];
+						}
 					}
 
 					break;
